c853441477d87b9e14794f2fae5fe058b09f5380
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-08-29 20:47:58 -0500

    48. Rotate Image explanation

--- rotate.py
+++ rotate.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-         def swap(array):
+        def swap(array):
             left = 0
             right = len(array)-1
             while left < right:
@@ -13,10 +13,14 @@ class Solution(object):
                 array[right] = temp
                 left += 1
                 right -= 1
+
+        #Tranpose of the matrix
         for i in range(len(matrix)):
             for j in range(i + 1, len(matrix)):
                 temp = matrix[i][j]
                 matrix[i][j] = matrix[j][i]
                 matrix[j][i] = temp
+
+        #Revert each matrix row after transpose
         for i in range(len(matrix)):
             swap(matrix[i])
