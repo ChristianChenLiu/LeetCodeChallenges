bf01b559c46042711cb89ceb911f62c7e19463a0
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-09-29 00:21:13 -0400

    61. Rotate List

--- RotateList.py
+++ RotateList.py
@@ -10,22 +10,28 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
+        #if we not need to rotate or linked list is empty, we just return head
         if k == 0 or head == None: return head
         
+        #we try to get the tail (last node in the linked list)
+        #and append all the nodes in the linked list to nodeList on the way
         tail = head
-        
         nodeList = [tail]
-        
         while tail.next != None:
             tail = tail.next
             nodeList.append(tail)
         
+        #we calculate the actual rotation required
         rotation =  k % len(nodeList)
         
+        #if rotation is actually 0 or linked list only have a single node,
+        #we just return the head
         if rotation == 0 or len(nodeList) == 1: return head
         
+        #Otherwise, we link the tail to the head
+        #then we get the new tail to point to None
+        #and return the node that was pointed to before from our new tail
         tail.next = head
-        
         nodeList[-rotation-1].next = None
         
         return nodeList[-rotation]
