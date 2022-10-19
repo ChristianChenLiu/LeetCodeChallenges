5857b1f43698c371263d081a49922a2e05685895
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-10-18 23:59:47 -0400

    692. Top K Frequent Words

---
+++ topKFrequent.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        dict = {}
+        for x in words:
+            if x in dict:
+                dict[x] += 1
+            else:
+                dict[x] = 1
+        res = sorted(dict, key=lambda x: (-dict[x], x))
+        return res[:k]
