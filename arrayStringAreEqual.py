cbb791053afee4a8276331d04a7c5c3a89c90c8f
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-10-25 19:59:37 -0400

    1662. Check If Two String Arrays are Equivalent

---
+++ arrayStringAreEqual.py
@@ -0,0 +1,7 @@
+def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+        return "".join(word1) == "".join(word2)
