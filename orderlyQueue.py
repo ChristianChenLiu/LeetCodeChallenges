a61700ff69992428df5c9ee28fb3240363c3dc96
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-11-05 23:47:22 -0400

    899. Orderly Queue explanation

--- orderlyQueue.py
+++ orderlyQueue.py
@@ -1,5 +1,7 @@
 class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
+        # if k == 1, then we can only shift and see which one gives us the best lexical order
         if k == 1:
             return min(s[i:] + s[:i] for i in range(len(s)))
+        #if k>1, then we can always get the lexical order
         return ''.join(sorted(s))
