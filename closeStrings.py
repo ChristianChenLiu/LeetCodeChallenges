14accda6d8770583ae00057b947b4782d3d2e957
Christian Chen Liu <Christian.chenliu@mail.utoronto.ca>
2022-12-01 20:13:51 -0500

    1657. Determine if Two Strings Are Close

---
+++ closeStrings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        w1 = Counter(word1)
+        w2 = Counter(word2)
+        return w1.keys() == w2.keys() and sorted(w1.values()) == sorted(w2.values())
